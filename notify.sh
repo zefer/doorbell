16d8adff9545c858bd1651d71fb5ec2afe8ce3bf
Joe Roberts <joe@zefer.co.uk>
2021-06-23 15:32:14 +0100

    Update the doorbell to pause/restart music
    Before: it paused the music
    After: it pauses the music, and if it _was_ playing, restarts it 10
    seconds later.

--- notify.sh
+++ notify.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-MPD_HOST=music mpc pause
-#./notify.py
+export MPD_HOST=music
+
+wasplaying=$(mpc status | grep "\[playing\]")
+
+mpc pause
+
+if [ -n "$wasplaying" ]; then
+  sleep 10
+  mpc play
+fi
 
 time="$(date -u +'%Y-%m-%dT%H:%M:%SZ')"
 echo "${time}" >> /var/log/doorbell.log
