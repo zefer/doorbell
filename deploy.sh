72214b1af0276ac005385135be560f8801b09bb6
Joe Roberts <joe@zefer.co.uk>
2021-06-23 15:05:45 +0100

    Fix doorbell notify
    Revert back to a simple "pause music" doorbell behaviour and put this
    back into version-controlled code. I'd been experimenting with a more
    clever system that played doorbell tones with a python script but it was
    not reliable and I want something reliable and working to revert to in
    version control.

--- deploy.sh
+++ deploy.sh
@@ -10,13 +10,22 @@ GOOS=linux GOARM=6 GOARCH=arm go build
 echo 'Sending binary to doorbell server'
 scp doorbell $server_host:/home/joe
 
+echo 'Archiving old notify.sh on doorbell server'
+ssh $server_host -t "\
+  sudo mv /opt/doorbell/notify.sh \
+  /opt/doorbell/notify.sh.bak.$(date -u +'%Y-%m-%dT%H-%M-%SZ')"
+
+echo 'Sending notify.sh to doorbell server'
+scp notify.sh $server_host:/home/joe
+
 echo 'Running commands on doorbell server'
 ssh $server_host -t '\
   sudo systemctl stop doorbell \
   && sleep 1 \
-  && sudo chown root /home/joe/doorbell \
-  && sudo chgrp root /home/joe/doorbell \
+  && sudo chown root /home/joe/doorbell /home/joe/notify.sh \
+  && sudo chgrp root /home/joe/doorbell /home/joe/notify.sh \
   && sudo mv /home/joe/doorbell /opt/doorbell \
+  && sudo mv /home/joe/notify.sh /opt/doorbell \
   && sudo systemctl start doorbell \
   && sleep 1
 '
--- notify.sh
+++ notify.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-mpc pause
-./notify.py
+MPD_HOST=music mpc pause
+#./notify.py
 
 time="$(date -u +'%Y-%m-%dT%H:%M:%SZ')"
 echo "${time}" >> /var/log/doorbell.log
